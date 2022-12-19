cfcaf711da435a0e3dfe9dde73097da20274408f
Xeon Xai <xeokeri@me.com>
2022-12-19 16:47:15 -0500

    Updating the deprecated to the new value.

--- keyboards/thok/bloq/keymaps/default/keymap.c
+++ keyboards/thok/bloq/keymaps/default/keymap.c
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
     [THOK_LAYER_RESET] = LAYOUT (
-                          RESET,
+                          QK_BOOT,
       XXXXXXX,  XXXXXXX,  XXXXXXX,
       XXXXXXX,  XXXXXXX,  XXXXXXX,
       XXXXXXX,  XXXXXXX,  XXXXXXX

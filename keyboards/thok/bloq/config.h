70fdf268ccea34c1a0f202dd5255d3b20e101ddb
Xeon Xai <xeokeri@me.com>
2022-12-19 16:55:08 -0500

    Updates for deprecations.

--- keyboards/thok/bloq/config.h
+++ keyboards/thok/bloq/config.h
@@ -19,11 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-#define VENDOR_ID 0x2011
-#define PRODUCT_ID 0x0001
-#define DEVICE_VER 0x0001
-#define MANUFACTURER THOK
-#define PRODUCT Bloq
 
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 3
